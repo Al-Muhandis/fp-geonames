e2a95177cfdca6472b56205b352e68ba17cf2de4
Al-Muhandis <mail@Renat.Su>
2017-11-23 21:19:35 +0400

    Create geonames.pas

---
+++ geonames.pas
@@ -0,0 +1,195 @@
+unit geonames;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpjson;
+
+type
+
+  TLogMessageEvent = procedure(Sender: TObject; LogType: TEventType; const Msg: String) of object;
+  TVerbosityStyle = (vsDefault, vsShort, vsMedium, vsLong, vsFull);
+
+  { TGeoNamesParser }
+
+  TGeoNamesParser = class
+  private
+    FGetParams: TStrings;
+    FJSONResponse: TJSONObject;
+    FFormatSettings: TFormatSettings;
+    FOnLogMessage: TLogMessageEvent;
+    FResponse: String;
+    FRequestBody: String;
+    FUserName: String;
+    procedure DebugMessage(const Msg: String);
+    procedure ErrorMessage(const Msg: String);
+    function GetJSONResponse: TJSONObject;
+    procedure InfoMessage(const Msg: String);
+    function SendAPIMethod(const AMethod: String): Boolean;
+    procedure SetLatitude(AValue: Double);
+    procedure SetLongitude(AValue: Double);
+    procedure SetRequestBody(AValue: String);
+    procedure SetStyle(AValue: TVerbosityStyle);
+    procedure SetUserName(AValue: String);
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function findNearby(ALatitude, ALongitude: Double): Boolean;
+    function findNearbyPlaceName(ALatitude, ALongitude: Double): Boolean;
+    function timezone(ALatitude, ALongitude: Double): Boolean;
+    property Latitude: Double write SetLatitude;
+    property Longitude: Double write SetLongitude;
+    property OnLogMessage: TLogMessageEvent read FOnLogMessage write FOnLogMessage;
+    property RequestBody: String read FRequestBody write SetRequestBody;
+    property Response: String read FResponse;
+    property UserName: String read FUserName write SetUserName;
+    property JSONResponse: TJSONObject read GetJSONResponse;
+    property Style: TVerbosityStyle write SetStyle;
+  published
+
+  end;
+
+implementation
+
+uses
+  fphttpclient, jsonscanner, jsonparser;
+
+const
+  api_url = 'http://api.geonames.org/';
+  VerbosityStyleStrings: array[TVerbosityStyle] of PChar = ('medium','short','medium','long','full');
+
+{ TGeoNamesParser }
+
+
+procedure TGeoNamesParser.DebugMessage(const Msg: String);
+begin
+  if Assigned(FOnLogMessage) then
+    FOnLogMessage(Self, etDebug, Msg);
+end;
+
+procedure TGeoNamesParser.ErrorMessage(const Msg: String);
+begin
+  if Assigned(FOnLogMessage) then
+    FOnLogMessage(Self, etError, Msg);
+end;
+
+function TGeoNamesParser.GetJSONResponse: TJSONObject;
+var
+  lParser: TJSONParser;
+begin
+  if not Assigned(FJSONResponse) then
+  begin
+    if FResponse<>EmptyStr then
+    begin
+      lParser := TJSONParser.Create(FResponse);
+      try
+        try
+          FJSONResponse := lParser.Parse as TJSONObject;
+        except
+          FJSONResponse := nil;
+        end;
+      finally
+        lParser.Free;
+      end;
+    end
+  end;
+  Result:=FJSONResponse;
+end;
+
+procedure TGeoNamesParser.InfoMessage(const Msg: String);
+begin
+  if Assigned(FOnLogMessage) then
+    FOnLogMessage(Self, etInfo, Msg);
+end;
+
+function TGeoNamesParser.SendAPIMethod(const AMethod: String): Boolean;
+var
+  HTTP: TFPHTTPClient;
+begin
+  HTTP:=TFPHTTPClient.Create(nil);
+  FGetParams.Values['username']:=FUserName;
+  try
+    try
+  //    AParams;                                          url encode???
+      FResponse:=HTTP.Get(API_URL+AMethod+'JSON'+'?'+FGetParams.DelimitedText);
+      Result:=True;
+    except
+      Result:=False;
+    end;
+  finally
+    HTTP.Free;
+  end;
+end;
+
+procedure TGeoNamesParser.SetLatitude(AValue: Double);
+begin
+  FGetParams.Values['lat']:=FloatToStr(AValue, FFormatSettings);
+end;
+
+procedure TGeoNamesParser.SetLongitude(AValue: Double);
+begin
+  FGetParams.Values['lng']:=FloatToStr(AValue, FFormatSettings);
+end;
+
+procedure TGeoNamesParser.SetRequestBody(AValue: String);
+begin
+  if FRequestBody=AValue then Exit;
+  FRequestBody:=AValue;
+end;
+
+procedure TGeoNamesParser.SetStyle(AValue: TVerbosityStyle);
+begin
+  FGetParams.Values['style']:=VerbosityStyleStrings[AValue]
+end;
+
+procedure TGeoNamesParser.SetUserName(AValue: String);
+begin
+  if FUserName=AValue then Exit;
+  FUserName:=AValue;
+end;
+
+constructor TGeoNamesParser.Create;
+begin
+  inherited Create;
+  FFormatSettings:=DefaultFormatSettings;
+  FFormatSettings.DecimalSeparator:='.';
+  FJSONResponse:=nil;
+  FGetParams:=TStringList.Create;
+  FGetParams.StrictDelimiter:=True;
+  FGetParams.Delimiter:='&';
+end;
+
+destructor TGeoNamesParser.Destroy;
+begin
+  if Assigned(FJSONResponse) then
+    FJSONResponse.Free;
+  FGetParams.Free;
+  inherited Destroy;
+end;
+
+function TGeoNamesParser.findNearby(ALatitude, ALongitude: Double): Boolean;
+begin
+  Latitude:=ALatitude;
+  Longitude:=ALongitude;
+  Result:=SendAPIMethod('findNearby');
+end;
+
+function TGeoNamesParser.findNearbyPlaceName(ALatitude, ALongitude: Double
+  ): Boolean;
+begin
+  Latitude:=ALatitude;
+  Longitude:=ALongitude;
+  Result:=SendAPIMethod('findNearbyPlaceName');
+end;
+
+function TGeoNamesParser.timezone(ALatitude, ALongitude: Double): Boolean;
+begin
+  Latitude:=ALatitude;
+  Longitude:=ALongitude;
+  Result:=SendAPIMethod('timezone');
+end;
+
+end.
+
