2902049e71c5dbf97590c4b4a0320856551198b5
Renat Suleymanov <mail@renat.su>
2024-12-11 23:49:56 +0400

    Add package

---
+++ geonames_pkg.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit geonames_pkg;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  geonames;
+
+implementation
+
+end.
